4e5edabb4661a76f0a31b7c84d7277b602501faf
PeterChou566 <peter820307@gmail.com>
2016-11-28 20:15:41 +0800

    Initial Commit

---
+++ 2TodayExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2TodayExtension
+//
+//  Created by 周大剛 on 2016/11/28.
+//  Copyright © 2016年 周大剛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
